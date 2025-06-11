e6acb59f88fcc608e581d2fd814199bd770ab15b
Junaid Shah <codedbyjunaid@gmail.com>
2025-06-11 06:27:19 +0500

    Add files via upload

---
+++ Lecture-1.py
@@ -0,0 +1,77 @@
+# Variables:
+
+# name = "Junaid"
+# age = 24
+# price = 24.99
+
+# print(name)
+# print(age)
+# print(price)
+
+# print(f"My name is {name}")
+# print(f"My age is {age}")
+# print(f"I have ${price} Shirt")
+
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+# name1 = 'sk'
+# name2 = "sk"
+# name3 = '''sk'''
+
+# print(name1)
+# print(name2)
+# print(name3)
+
+
+# Data Types
+
+# i)      Integer  (+ve, -ve, 0)
+# ii)     String   ("Junaid", "Hello")
+# iii)    Float    (3.9, 3.7)
+# iv)     Boolean  (True, False)
+# v)      None     (a = None)
+
+
+# Operators
+
+# 1: Arithimatic Operator:
+
+a = 5
+b = 2
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a**b)
+
+# Relational Operators
+
+# print(a==b)
+# print(a!=b)
+# print(a<b)
+# print(a>b)
+# print(a<=b)
+# print(a>=b)
+
+
+# Qs. Write a program to input 2 numbers & print their sum.
+
+# a  = int(input("Enter your first Number "))
+# b  = int(input("Enter your second Number "))
+
+# sum = a + b
+
+# print(sum)
+
+
+
+# Qs. WAP to input side of a square & print its area.
+
+# Area = float(input("Enter your number "))
+# side = Area * Area
+
+# print(side)
